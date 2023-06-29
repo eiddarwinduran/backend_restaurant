ef87cdee4b29b01e804245d8b11bb4db4f38762d
eiddarwinduran <eiddarwind734@gmail.com>
2023-06-29 18:23:28 -0400

    Correciones1

--- src/pedido/entities/pedido.entity.ts
+++ src/pedido/entities/pedido.entity.ts
@@ -1,12 +1,11 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+
 @Entity('pedidos')
 export class PedidoEntity {
   @PrimaryGeneratedColumn()
   id: number;
-  
-  //@Column({name: 'id_platillos'})
-  //idPlatillo:number;
+
   @Column()
   nombreC:string;
 
@@ -20,10 +19,6 @@ export class PedidoEntity {
   cantidad:number;
 
 
-  @CreateDateColumn({ name: 'fecha_pedido' })
+  @CreateDateColumn()
   fechaPedido: Date;
-
-  //@ManyToOne(() => PlatilloEntity, (platillo) => platillo.pedido)
-  //@JoinColumn({ name: 'idPlatillo', referencedColumnName: 'id' })
-  //platillo: PlatilloEntity;
 }
